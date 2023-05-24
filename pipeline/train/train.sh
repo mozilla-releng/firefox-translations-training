bef039ffab2ad6c2cc205cab20e80c8d9c6bc007
Ben Hearsum <ben@mozilla.com>
2023-05-24 15:53:53 -0400

    train backwards pipeline script

--- pipeline/train/train.sh
+++ pipeline/train/train.sh
@@ -19,6 +19,8 @@ vocab=$8
 best_model_metric=$9
 extra_params=( "${@:10}" )
 
+ARTIFACT_EXT="${ARTIFACT_EXT:-gz}"
+
 test -v GPUS
 test -v MARIAN
 test -v WORKSPACE
@@ -32,10 +34,16 @@ echo "### Training ${model_dir}"
 
 # if doesn't fit in RAM, remove --shuffle-in-ram and add --shuffle batches
 
+# TODO: remove this hack
+zstdmt --rm -d "${train_set_prefix}.${src}.${ARTIFACT_EXT}"
+zstdmt --rm -d "${train_set_prefix}.${trg}.${ARTIFACT_EXT}"
+zstdmt --rm -d "${valid_set_prefix}.${src}.${ARTIFACT_EXT}"
+zstdmt --rm -d "${valid_set_prefix}.${trg}.${ARTIFACT_EXT}"
+
 "${MARIAN}/marian" \
   --model "${model_dir}/model.npz" \
   -c "configs/model/${model_type}.yml" "configs/training/${model_type}.${training_type}.yml" \
-  --train-sets "${train_set_prefix}".{"${src}","${trg}"}.gz \
+  --train-sets "${train_set_prefix}".{"${src}","${trg}"} \
   -T "${model_dir}/tmp" \
   --shuffle-in-ram \
   --vocabs "${vocab}" "${vocab}" \
@@ -44,7 +52,7 @@ echo "### Training ${model_dir}"
   --sharding local \
   --sync-sgd \
   --valid-metrics "${best_model_metric}" ${all_model_metrics[@]/$best_model_metric} \
-  --valid-sets "${valid_set_prefix}".{"${src}","${trg}"}.gz \
+  --valid-sets "${valid_set_prefix}".{"${src}","${trg}"} \
   --valid-translation-output "${model_dir}/devset.out" \
   --quiet-translation \
   --overwrite \

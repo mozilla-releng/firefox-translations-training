e3faacd080725d14842161a647b978b32b3365d1
Ben Hearsum <ben@mozilla.com>
2023-06-01 16:58:37 -0400

    hack

--- taskcluster/scripts/toolchain/build-marian.sh
+++ taskcluster/scripts/toolchain/build-marian.sh
@@ -5,6 +5,10 @@ set -x
 export MARIAN_DIR=$MOZ_FETCHES_DIR/marian-source
 export CUDA_DIR=$MOZ_FETCHES_DIR/cuda-toolkit
 
+# Ugly hack to workaround the fact that browsermt Marian doesn't have proper CUDA 12.0 support
+# in its build system.
+sed -i -e 's/"Compile GPU version with SM35 support" ON/"Compile GPU version with SM35 support" OFF/' $MARIAN_DIR/CMakeLists.txt
+
 # TODO: consider not calling out to this since it's such a simple script...
 bash $VCS_PATH/pipeline/setup/compile-marian.sh "${MARIAN_DIR}/build" "$(nproc)"
 

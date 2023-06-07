a6d9d54817c531055d42c8bf68aa24fc24cfc498
Ben Hearsum <ben@mozilla.com>
2023-06-07 12:20:05 -0400

    Implement `evaluate-finetuned-teacher` step
    Yet another ensembled step!

--- taskcluster/translations_taskgraph/actions/train.py
+++ taskcluster/translations_taskgraph/actions/train.py
@@ -38,7 +38,7 @@ defaults = get_defaults("")["training_config"]
 (any stages this choice depends on will be automatically included).""",
                 "default": defaults["target-stage"],
                 # TODO: this should probably be specified in ci/config.yml
-                "enum": ["clean-corpus", "clean-mono", "bicleaner", "bicleaner-ai", "merge-corpus", "merge-devset", "train-vocab", "train-backwards", "evaluate-backwards", "train-teacher", "evaluate-teacher", "finetune-teacher"],
+                "enum": ["clean-corpus", "clean-mono", "bicleaner", "bicleaner-ai", "merge-corpus", "merge-devset", "train-vocab", "train-backwards", "evaluate-backwards", "train-teacher", "evaluate-teacher", "finetune-teacher", "evaluate-finetuned-teacher"],
             },
             "experiment": {
                 "type": "object",

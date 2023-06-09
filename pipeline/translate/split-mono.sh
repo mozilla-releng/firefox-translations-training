0e2b0448f2f0e6598baec4e36c43dbd3414d14be
Ben Hearsum <ben@mozilla.com>
2023-06-09 13:25:12 -0400

    pipeline

--- pipeline/translate/split-mono.sh
+++ pipeline/translate/split-mono.sh
@@ -12,6 +12,19 @@ test -v BIN
 mono_path=$1
 output_dir=$2
 length=$3
+compress_output=${4:-false}
+
+COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
+ARTIFACT_EXT="${ARTIFACT_EXT:-gz}"
+
+if [ "${compress_output}" = "" ]; then
+  compress_output="false"
+fi
 
 mkdir -p "${output_dir}"
-pigz -dc "${mono_path}" | ${BIN}/dedupe | split -d -l ${length} - "${output_dir}/file."
+${COMPRESSION_CMD} -dc "${mono_path}" | ${BIN}/dedupe | split -d -l ${length} - "${output_dir}/file."
+
+if [ "${compress_output}" = "true" ]; then
+  cd "${output_dir}"
+  tar -cf - * | $COMPRESSION_CMD > "${output_dir}/split-files.tar.${ARTIFACT_EXT}"
+fi

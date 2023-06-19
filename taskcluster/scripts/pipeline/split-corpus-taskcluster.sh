cfa9f9be54a3a983fd63327a200ae8026c2c1695
Ben Hearsum <ben@mozilla.com>
2023-06-19 19:54:31 -0400

    Fix split wrapper scripts

--- taskcluster/scripts/pipeline/split-corpus-taskcluster.sh
+++ taskcluster/scripts/pipeline/split-corpus-taskcluster.sh
@@ -3,24 +3,16 @@
 set -x
 set -euo pipefail
 
-type=$1
-chunks=$2
-output_dir=$3
-length=$4
-lang_args=( "${@:5}" )
+chunks=$1
+output_dir=$2
+length=$3
+lang_args=( "${@:4}" )
 
 pushd `dirname $0`/../../.. &>/dev/null
 VCS_ROOT=$(pwd)
 popd &>/dev/null
 
-if [ "${type}" = "mono" ]; then
-  ${VCS_ROOT}/pipeline/translate/split-mono.sh "${lang_args[@]}" "${output_dir}" "${length}"
-elif [ "${type}" = "corpus" ]; then
-  ${VCS_ROOT}/pipeline/translate/split-corpus.sh "${lang_args[@]}" "${output_dir}" "${length}"
-else
-  echo "Unknown split type: ${type}"
-  exit 1
-fi
+${VCS_ROOT}/pipeline/translate/split-mono.sh "${lang_args[@]}" "${output_dir}" "${length}"
 
 # Taskcluster requires a consistent number of chunks; split the resulting files
 # evenly into the requested number of chunks, creating empty archives if there's
@@ -30,13 +22,14 @@ ls file* | grep -v "\.ref" | sort > src-files.txt
 ls file* | grep "\.ref" | sort > ref-files.txt
 for i in $(seq 1 ${chunks} | tr '\n' ' '); do
   src_files=$(split -n l/${i}/${chunks} src-files.txt | tr '\n' ' ')
-  ref_files=$(split -n l/${i}/${chunks} ref-files.txt | tr '\n' ' ')
   if [ "${src_files}" = "" ]; then
     touch "src-file.${i}"
   else
     cat ${src_files} > "src-file.${i}"
   fi
   zstd --rm "src-file.${i}"
+
+  ref_files=$(split -n l/${i}/${chunks} ref-files.txt | tr '\n' ' ')
   if [ "${ref_files}" = "" ]; then
     touch "ref-file.${i}"
   else
